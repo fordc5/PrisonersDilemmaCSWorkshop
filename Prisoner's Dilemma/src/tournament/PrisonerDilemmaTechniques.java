ff7584c9af5c88763b576a0ca3356ad592356595
Nolan McCafferty <njma2016@pomona.edu>
2017-11-02 18:19:47 -0700

    see if this works lol

--- Prisoner's Dilemma/src/tournament/PrisonerDilemmaTechniques.java
+++ Prisoner's Dilemma/src/tournament/PrisonerDilemmaTechniques.java
@@ -31,6 +31,7 @@ public class PrisonerDilemmaTechniques {
 	private Randy randy = new Randy();
 	private Squire squire = new Squire();
 	private HitForHat hFH = new HitForHat();
+	private Strat47 strat = new Strat47();
 	
 	
 	public PrisonerDilemmaTechniques() {
@@ -42,6 +43,7 @@ public class PrisonerDilemmaTechniques {
 		strategies.add(randy);
 		strategies.add(squire);
 		strategies.add(hFH);
+		strategies.add(strat);
 	}
 
 
---
+++ Prisoner's Dilemma/src/tournament/strategies/Strat47.java
@@ -0,0 +1,12 @@
+package tournament.strategies;
+
+import tournament.Strategy;
+import tournament.Turn;
+import java.util.ArrayList;
+
+public class Strat47 implements Strategy {
+
+	public String takeOneTurn(ArrayList<Turn<String,String>> history) {
+		return "d";
+	}
+}
